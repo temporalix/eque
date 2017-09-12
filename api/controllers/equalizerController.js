35c600b5ed24cd6c56bc87e5bcba0ceba98f6810
Brandon <brandon.gore@kaleidocode.co.za>
2017-09-12 23:58:18 +0200

    add more tests for bad input

--- api/controllers/equalizerController.js
+++ api/controllers/equalizerController.js
@@ -1,10 +1,10 @@
 'use strict';
 
 exports.equalize = function (req, res, next) {
-    // if(req.body){
-    // var err= {success: false, message: "no data sent"}
-    //  return res.send(err);
-    // }
+    if(isEmptyObject(req.body)){
+            res.status(400);
+      return res.send('no data sent');
+    }
     calculate(req.body, function (err, result) {
         console.log(err);
          if (err) {
@@ -16,26 +16,37 @@ exports.equalize = function (req, res, next) {
     });
 };
 
+function isEmptyObject(obj) {
+  for (var key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      return false;
+    }
+  }
+  return true;
+}
+
 function calculate(data, callback) {
-     var err;
-    if (data.people === undefined || data.rows === undefined || data.rows.length < 1) {
-          err={message: "invalid entries"};
-       return callback(err, 0);
-    }
+   
+      var err;
+       return callback(err, [0]);
+    // if (data.people === undefined || data.rows === undefined || data.rows.length < 1) {
+    //       err={message: "invalid entries"};
+    //    return callback(err, 0);
+    // }
 
-    var result = 0;
-    if (data.people < 2) {
-        return callback(err, result);
-    }
+    // var result = 0;
+    // if (data.people < 2) {
+    //     return callback(err, result);
+    // }
 
-    var dataTotal = 0;
-    if (data.rows.length)
-        data.rows.forEach(function (item) {
-            dataTotal = dataTotal + item;
-        });
+    // var dataTotal = 0;
+    // if (data.rows.length)
+    //     data.rows.forEach(function (item) {
+    //         dataTotal = dataTotal + item;
+    //     });
 
-    result = ((dataTotal / data.people) * (data.people - 1));
-    callback(err, result);
+    // result = ((dataTotal / data.people) * (data.people - 1));
+    // callback(err, result);
 }
 
 function readData(data) {
--- test/server.test.js
+++ test/server.test.js
@@ -33,42 +33,60 @@ let server = require('../server');
 let should = chai.should();
 
 chai.use(chaiHttp);
-
+chai.use(require('chai-like'));
+chai.use(require('chai-things'));
 // var calc = require("../app/scripts/calc");
 
-describe("Server", function(){
-    it("Accessible", function(done){
+describe("Server", function () {
+    it("should return error when no parameters sent", function (done) {
         chai.request(server)
-        .post('/')
-        //.send({ people: 3, rows:[10,20,30]})
-        .end(function(err, res){
-            res.should.have.status(200);
-            //err.should.be.null;
-            expect(err).to.eql({success: false, message: "no data sent"});
-           // console.log(res.body);
-            // res.body.should.be.a('array');
-            // res.body.length.should.be.eql(0);
-            done();
-        })
-        // expect(calc.equilize(0, [])).to.equal(0);
-        //  done();
+            .post('/')
+            .end(function (err, res) {
+                res.should.have.status(400);
+                expect(err).to.not.be.null;
+                expect(res.text).to.eql("no data sent");
+                done();
+            });
     });
 
-    it("should return zero when for 1 person", function(done){
+    it("should return error when the number of students is greater than 1000", function (done) {
         chai.request(server)
-        .post('/')
-        .send({ people: 1, rows:[10,20,30]})
-        .end(function(err, res){
-            res.should.have.status(200);
-            // err.should.be.null;
-            // res.body.should.be.a('array');
-            // res.body.should.be.eql(0);
-            done();
-        })
-        // expect(calc.equilize(0, [])).to.equal(0);
-        //  done();
-    })
+            .post('/')
+            .send([{ people: 1001, rows: [10, 20, 30] }])
+            .end(function (err, res) {
+                 res.should.have.status(400);
+                expect(err).to.not.be.null;
+                expect(res.text).to.eql("no data sent");
+                done();
+            });
+    });
 
+        it("should return error when the amount spent by a student is greater than 10000", function (done) {
+        chai.request(server)
+            .post('/')
+            .send([{ people: 1, rows: [5000,5001] }])
+            .end(function (err, res) {
+                 res.should.have.status(400);
+                expect(err).to.not.be.null;
+                expect(res.text).to.eql("no data sent");
+                done();
+            });
+    });
+
+
+    it("should return zero when for 1 person with data", function (done) {
+        chai.request(server)
+            .post('/')
+            .send([{ people: 1, rows: [10, 20, 30] }])
+            .end(function (err, res) {
+                res.should.have.status(200);
+                expect(err).to.be.null;
+                res.body.should.be.a('array');
+                res.body.length.should.be.eql(1);
+                expect(res.body).to.deep.equal([0]);
+                done();
+            });
+    });
     // it("returns zero when only 1 person", function(done){
     //     expect(calc.equilize(0, [10, 20])).to.equal(0);
     //      done();
